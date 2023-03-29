3706d6534e2a0bc7f334810f11382d4bca13ee07
AntonioMarcosDev <ambrcz@hotmail.com>
2023-03-29 18:05:12 -0300

    Adicionando BMExercicio 2

---
+++ BMExercicio02.java
@@ -0,0 +1,25 @@
+/**
+ * 2. Escreva um programa que declare, inicialize e imprima as seguintes variáveis:
+ * • Inteiro i de 32 bits com valor 1
+ * • Inteiro j de 64 bits com valor 2
+ * • Ponto flutuante p de 32 bits com valor 20.0
+ * • Ponto flutuante q de 64 bits com valor 30.0
+ * • Boolean b com valor verdadeiro
+ * • Caracter c com valor ‘k’
+ */
+public class BMExercicio02 {
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println(i);
+        long j = 2;
+        System.out.println(j);
+        float p = 20.0F;
+        System.out.println(p);
+        double q = 30.0;
+        System.out.println(q);
+        boolean b = true;
+        System.out.println(b);
+        char c = 'k';
+        System.out.println(c);
+    }
+}
