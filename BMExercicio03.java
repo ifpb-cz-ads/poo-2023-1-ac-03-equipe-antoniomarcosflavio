5f55ae4b16a4b7a9166b48d7db3e20b13d9f7f6b
AntonioMarcosDev <ambrcz@hotmail.com>
2023-03-29 19:29:17 -0300

    Formatando BMExercicio 3

--- BMExercicio03.java
+++ BMExercicio03.java
@@ -1,9 +1,7 @@
 /**
  * BMExercicio3
  */
-public class BMExercicio3 {
-    static final double taxa = 8.25/100;
-
+public class BMExercicio03 {
     public static void main(String[] args) {
         // letra A
         double precoItem1 = 2.95, precoItem2 = 3.50;
@@ -13,20 +11,21 @@ public class BMExercicio3 {
 
         // letra C
         double custoTotalDosItens = precoItem1 + precoItem2;
-        System.out.println("A soma do preco dos itens é de " + custoTotalDosItens); 
+        System.out.println("A soma do preco dos itens é de " + custoTotalDosItens);
 
         // letra D
+        final double taxa = 8.25 / 100;
         double taxaCalculada = custoTotalDosItens * taxa;
         double taxaTotal = taxaCalculada + custoTotalDosItens;
-        System.out.printf("A taxa cobrada será de %.2f e somada ao valor do custo total dos itens será de %.2f.\n",taxaCalculada,taxaTotal);
+        System.out.printf("A taxa cobrada será de %.2f e somada ao valor do custo total dos itens será de %.2f.\n", taxaCalculada, taxaTotal);
 
         // letra E
-        precoItem1 += taxaCalculada; precoItem2 += taxaCalculada;
+        precoItem1 += taxaCalculada;
+        precoItem2 += taxaCalculada;
         double novoCusto = precoItem1 + precoItem2;
 
         // letra F
         boolean muitoCaro = (novoCusto > 10);
         System.out.println(muitoCaro);
     }
-    
 }
