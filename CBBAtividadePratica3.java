e49af5bf2ebdbf2e59fe85079701bfe2ec5b8e34
Flavio Henrique <chakcpps@gmail.com>
2023-03-29 17:10:34 -0300

    Adicionando atividade prática 3

---
+++ CBBAtividadePratica3.java
@@ -0,0 +1,13 @@
+/**
+ * CBBAtividadePratica3
+ * Crie um programa que calcule a área de um retângulo, declare e inicialize duas variáveis para testá-lo,
+ * imprimindo esses resultados na tela.
+ */
+public class CBBAtividadePratica3 {
+    public static void main(String[] args) {
+        int base = 10, altura = 7;
+        int areaDoRetangulo = base * altura;
+
+        System.out.printf("A base e a altura de um retangulo são respectivamente %d e %d, logo sua área é %d.",base, altura,areaDoRetangulo);
+    }
+}
