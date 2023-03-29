0cff9a018e8c296d3db496569c8180ff412a0532
Flavio Henrique <chakcpps@gmail.com>
2023-03-29 17:11:01 -0300

    Adicionando BMExercicio 3

---
+++ BMExercicio3.java
@@ -0,0 +1,32 @@
+/**
+ * BMExercicio3
+ */
+public class BMExercicio3 {
+    static final double taxa = 8.25/100;
+
+    public static void main(String[] args) {
+        // letra A
+        double precoItem1 = 2.95, precoItem2 = 3.50;
+
+        // letra B
+        System.out.println("O item 1 custa " + precoItem1 + " e o item 2 custa " + precoItem2);
+
+        // letra C
+        double custoTotalDosItens = precoItem1 + precoItem2;
+        System.out.println("A soma do preco dos itens é de " + custoTotalDosItens); 
+
+        // letra D
+        double taxaCalculada = custoTotalDosItens * taxa;
+        double taxaTotal = taxaCalculada + custoTotalDosItens;
+        System.out.printf("A taxa cobrada será de %.2f e somada ao valor do custo total dos itens será de %.2f.\n",taxaCalculada,taxaTotal);
+
+        // letra E
+        precoItem1 += taxaCalculada; precoItem2 += taxaCalculada;
+        double novoCusto = precoItem1 + precoItem2;
+
+        // letra F
+        boolean muitoCaro = (novoCusto > 10);
+        System.out.println(muitoCaro);
+    }
+    
+}
